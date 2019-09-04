4e0226a601b7da52c2f1c37156b0fb8a02cc2b57
ian Githungo <githungoian53@gmail.com>
2019-09-04 15:16:35 +0300

    django blog app

---
+++ DJANGO/user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+# urls for the user app
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done')
+]
